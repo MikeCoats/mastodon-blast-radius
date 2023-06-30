e55d64531214c268c16c97bd6e765a9690f15ff9
Mike Coats <i.am@mikecoats.com>
2023-06-30 19:28:13 +0000

    Add followers lookup API endpoint
    Includes a utility function to get `application/activity+json` format
    data from a mastodon server. Includes a method for recursively
    downloading a paged OrderedCollection.

--- server.mjs
+++ server.mjs
@@ -5,6 +5,34 @@ import express from 'express'
 const app = express()
 
 /**
+ *
+ * @param {string} url
+ * @returns {Promise<any>}
+ */
+const getActivityJson = async (url) => {
+  const activityHeaders = { headers: [['Accept', 'application/activity+json']] }
+  const response = await fetch(url, activityHeaders)
+  const activityJson = await response.json()
+  return activityJson
+}
+
+/**
+ *
+ * @param {*} url
+ * @returns
+ */
+const getPagedOrderedCollection = async (url) => {
+  const page = await getActivityJson(url)
+
+  if (!page.next) {
+    return page.orderedItems
+  }
+
+  const nextPage = await getPagedOrderedCollection(page.next)
+  return [page.orderedItems, nextPage].flat()
+}
+
+/**
  * Our server only relies on upstream HTTP connections, no databases or
  * caches, etc, so as long as we're up and can make outgoing web
  * requests, we're 'OK'.
@@ -37,11 +65,32 @@ app.get('/user', async (req, res) => {
   // Try to grab the user as a JSON object. If we fail for any reason
   // tell the user.
   try {
-    const response = await fetch(userUrl, { headers: [['Accept', 'application/activity+json']] })
-    const user = await response.json()
+    const user = await getActivityJson(userUrl)
     res.status(200).send({ user })
   } catch (_err) {
     res.status(500).send({ status: `Failed to lookup handle '${userUrl}'` })
+  }
+})
+
+/**
+ * /followers?handle=https://mikecoats.social/users/mike
+ * @param {string} handle
+ */
+app.get('/followers', async (req, res) => {
+  // Missing handles mean no lookups!
+  const userUrl = req.query.handle
+  if (userUrl === undefined || userUrl === '') {
+    res.status(404).send({ status: 'Cannot lookup user\'s followers without handle.' })
+    return
+  }
+
+  try {
+    const user = await getActivityJson(userUrl)
+    const followersInfo = await getActivityJson(user.followers)
+    const followers = await getPagedOrderedCollection(followersInfo.first)
+    res.status(200).send({ followers })
+  } catch (_err) {
+    res.status(500).send({ status: `Failed to lookup followers for handle '${userUrl}'` })
   }
 })
 
